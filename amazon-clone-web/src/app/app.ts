6c02f581f97652407ab9ccbbc01c4953a43cc186
Matheus Folle <mathfolle@icloud.com>
2026-09-16 16:26:05 -0300

    feat: torna lista de produtos reativa

--- amazon-clone-web/src/app/app.ts
+++ amazon-clone-web/src/app/app.ts
@@ -1,26 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.html',
-  styleUrls: ['./app.scss']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.html',
+    styleUrls: ['./app.scss']
 })
 export class App implements OnInit {
-  // guarda os produtos que chegarem da API
-  products: any[] = [];
+    // Transformamos o array em um Signal reativo
+    products = signal<any[]>([]);
 
-  ngOnInit() {
-    this.carregarProdutos();
-  }
-
-  // Faz a chamada nativa e limpa para o Spring Boot
-  async carregarProdutos() {
-    try {
-      const response = await fetch('http://localhost:8080/api/products');
-      this.products = await response.json();
-    } catch (error) {
-      console.error('Erro de conexão com o middleware:', error);
+    ngOnInit() {
+        this.carregarProdutos();
     }
-  }
+    // Faz a chamada nativa e limpa para o Spring Boot
+    async carregarProdutos() {
+        try {
+            const response = await fetch('http://localhost:8080/api/products');
+            const data = await response.json();
+
+            // Atualizamos o valor do Signal, forçando a tela a renderizar
+            this.products.set(data);
+        } catch (error) {
+            console.error('Erro de conexão com o middleware:', error);
+        }
+    }
 }
